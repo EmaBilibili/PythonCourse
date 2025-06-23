3b96584f26923204995e1c132205c9bb333a891d
Emanuel Binimelis <63335996+EmaBilibili@users.noreply.github.com>
2025-06-23 18:57:54 -0300

    Ejercicio Diccionarios en Python 1
    Crea un diccionario llamado mi_diccionario que contenga los siguientes tres pares de clave-valor, donde las claves sean los nombres de tres  frutas, y los valores sean su color principal.

---
+++ Ejercicio Diccionarios en Python 1.py
@@ -0,0 +1,8 @@
+mi_diccionario = {
+    "manzana": "roja",
+    "banana": "amarilla",
+    "naranja": "naranja"
+}
+
+# Puedes imprimir el diccionario para verificar su contenido
+print(mi_diccionario)
